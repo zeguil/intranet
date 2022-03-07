89c6cd23b191fe58080689074e25d97f44d48178
zeguil <zeguilhermelins@hotmail.com>
2022-03-07 14:00:46 -0400

    att2

--- backend/users/viewsets.py
+++ backend/users/viewsets.py
@@ -1,11 +1,12 @@
 from .models import User
 from .serializers import UserSerializer
-from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
 class UsuariosViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAuthenticated, )
+
     
